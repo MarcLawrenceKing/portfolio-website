449465fcebe88f725e91a3d794cd20ddc801a0a5
MarcLawrenceKing <124873963+MarcLawrenceKing@users.noreply.github.com>
2025-04-07 16:18:26 +0800

    improved code stucture for resume

--- client/src/pages/Resume.jsx
+++ client/src/pages/Resume.jsx
@@ -6,6 +6,45 @@ import Education from "../components/Education";
 import SkillsLanguages from "../components/SkillsLanguages";
 
 const Resume = () => {
+  const certifications = [
+    {
+      year: "2023",
+      title: "30 Days of Frontend",
+      org: "AWS Cloud Clubs",
+      place: "Manila City",
+      description:
+        "Built a strong foundation in front-end web development with HTML, CSS, and JavaScript by creating small, hands-on projects that put theory into practice.",
+    },
+    {
+      year: "2024",
+      title: "Java Programming",
+      org: "Joysis TECH VOC INC.",
+      place: "Malabon City",
+      description:
+        "Reviewed core programming concepts and gained experience with Object-Oriented Programming (OOP), the MVC design pattern, and JDBC for MySQL database connectivity. Applied these skills by building a simple CRUD application as a final group project.",
+    },
+    {
+      year: "2024",
+      title: "Data Management Workshop",
+      org: "MapaKalamidadPH",
+      place: "Quezon City",
+      description:
+        "Learned the importance of open-source data extraction for disaster response. Utilized an open-source API providing JSON data to visualize flood patterns in Tableau.",
+    },
+  ];
+
+  const education = [
+    {
+      year: "2022-2026",
+      school: "Polytechnic University of the Philipiines Manila",
+      place: "Manila City",
+      degree: "Undergraduate",
+      course: "BS Information Technology",
+      description:
+        "DOST RA 7687 scholar and consistent president lister, actively engaged in student tech organizations. Took on leadership roles and contributed to initiatives while exploring diverse IT fields including web development, cloud computing, data science, and cybersecurity.",
+    },
+  ];
+
   const skills = ["Web Development", "Data Science"];
 
   const languages = [
@@ -32,48 +71,21 @@ const Resume = () => {
         </a>
       </div>
       <div className="flex flex-col gap-10">
-        <ResumeCard>
-          <Certification
-            year="2023"
-            title="30 Days of Frontend"
-            org="AWS Cloud Clubs"
-            place="Manila City"
-            description="Built a strong foundation in front-end web development with HTML, CSS, and JavaScript by creating small, hands-on projects that put theory into practice. "
-          />
-        </ResumeCard>
-        <ResumeCard>
-          <Certification
-            year="2024"
-            title="Java Programming"
-            org="Joysis TECH VOC INC."
-            place="Malabon City"
-            description="Reviewed core programming concepts and gained experience with Object-Oriented Programming (OOP), the MVC design pattern, and JDBC for MySQL database connectivity. Applied these skills by building a simple CRUD application as a final group project."
-          />
-        </ResumeCard>
-        <ResumeCard>
-          <Certification
-            year="2024"
-            title="Data Management Workshop"
-            org="MapaKalamidadPH"
-            place="Quezon City"
-            description="Learned the importance of open-source data extraction for disaster response. Utilized an open-source API providing JSON data to visualize flood patterns in Tableau."
-          />
-        </ResumeCard>
+        {certifications.map((certification, index) => (
+          <ResumeCard>
+            <Certification key={index} {...certification} />
+          </ResumeCard>
+        ))}
       </div>
       <div className="flex items-center justify-center mb-5 mt-5 w-125 sm:justify-start md:w-160 lg:w-180 xl:w-200">
         <p className="text-resume-header text-accent2">Education</p>
       </div>
       <div className="flex flex-col gap-10">
-        <ResumeCard>
-          <Education
-            year="2022-2026"
-            school="Polytechnic University of the Philipiines Manila"
-            place="Manila City"
-            degree="Undergraduate"
-            course="BS Information Technology"
-            description="DOST RA 7687 scholar and consistent president lister, actively engaged in student tech organizations. Took on leadership roles and contributed to initiatives while exploring diverse IT fields including web development, cloud computing, data science, and cybersecurity."
-          />
-        </ResumeCard>
+        {education.map((educationz, index) => (
+          <ResumeCard>
+            <Education key={index} {...educationz} />
+          </ResumeCard>
+        ))}
         <ResumeCard className="flex gap-12 flex-col mt-14">
           <SkillsLanguages logo="hammer" name="Skills" items={skills} />
           <SkillsLanguages logo="code-xml" name="Languages" items={languages} />
