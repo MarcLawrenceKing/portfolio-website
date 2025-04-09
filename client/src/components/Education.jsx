e3b26331f35a11dfed9b8ac7a51aa150fc841f7b
MarcLawrenceKing <124873963+MarcLawrenceKing@users.noreply.github.com>
2025-04-09 23:37:31 +0800

    fix resume misalignmen

--- client/src/components/Education.jsx
+++ client/src/components/Education.jsx
@@ -1,9 +1,11 @@
 const Education = ({ year, school, place, degree, course, description }) => {
   return (
     <div className="flex flex-col justify-between gap-4 lg:flex-row">
-      <div className="flex flex-col flex-1 gap-1 items-center bg-accent5 rounded-2xl lg:items-start  p-5">
+      <div className="flex flex-col flex-1 gap-1 items-center bg-accent5 rounded-2xl lg:items-start p-5">
         <p className="mb-1 font-bold text-accent2 text-center">{year}</p>
-        <p className="text-[0.875rem] font-bold text-center">{school}</p>
+        <p className="text-[0.875rem] font-bold text-center lg:text-start">
+          {school}
+        </p>
         <p className="text-[0.875rem] text-gray text-center">{place}</p>
         <p className="text-[0.875rem] text-gray italic text-center">{degree}</p>
         <p className="text-[0.875rem] text-gray italic text-center">{course}</p>
